991e14449be2b495f57645fd7d47403acacd0293
Tyler Ervin <156231684+tylererv@users.noreply.github.com>
2025-10-03 20:52:22 -0400

    Initial commit

---
+++ lab1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lab1.h"
+/*
+ * readString - reads a line from a file given by
+ * fileName and returns it as a c-string.  The line
+ * return is removed from the string.
+ * 
+ * fileName is c-string representing the file to read.
+ * 
+ */
+char* readString(char* fileName){
+    //TODO: Replace this line with your code
+}
+
+/*
+ * mysteryExplode - takes a string of characters and explodes it
+ * as follows:
+ * 
+ * A non-empty string such as "Code" is turned into "CCoCodCode".
+ *
+ *   Return a new string similar to the string given above.
+ *
+ *  Example:
+ *   mysteryExplosion("Code") --> "CCoCodCode"
+ *   mysteryExplosion("abc") --> "aababc"
+ *   mysteryExplosion(":)") --> "::)"
+ * 
+ */
+char* mysteryExplode(const char* str){
+    //TODO: Replace this line with your code
+}
